28048a59abe2cd7f309f6f46581c40ed8774540f
tarikulabir <tarikulabir931@gmail.com>
2024-10-16 19:52:45 +0600

    1st commint

---
+++ lib/login-registration/registration.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class registration extends StatelessWidget {
+  const registration({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Registration"),
+      ),
+      body: Container(),
+    );
+  }
+}
