c07709a65780139e617213a442d52b920a1c17fc
tarikulabir <tarikulabir931@gmail.com>
2024-11-07 00:58:54 +0600

    Appointment

--- lib/widgets/home/appointment.dart
+++ lib/widgets/home/appointment.dart
@@ -1,8 +1,24 @@
 import 'package:car_01/widgets/finance/notification.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class appointment extends StatelessWidget {
+class appointment extends StatefulWidget {
   const appointment({super.key});
+
+  @override
+  State<appointment> createState() => _appointmentState();
+}
+
+class _appointmentState extends State<appointment> {
+  late DateTime _selectedDay;
+  late DateTime _focusedDay;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusedDay = DateTime.now();
+    _selectedDay = _focusedDay;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -89,6 +105,36 @@ class appointment extends StatelessWidget {
                   return;
                   (value != null && value.contains('@'));
                 },
+              ),
+              TableCalendar(
+                firstDay: DateTime.utc(2020, 1, 1),
+                lastDay: DateTime.utc(2030, 12, 31),
+                focusedDay: _focusedDay,
+                calendarFormat: CalendarFormat.month,
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                },
+                headerStyle: HeaderStyle(
+                  formatButtonVisible: false,
+                  titleCentered: true,
+                ),
+                calendarStyle: CalendarStyle(
+                  isTodayHighlighted: true,
+                  selectedDecoration: BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                  ),
+                  todayDecoration: BoxDecoration(
+                    color: Colors.orangeAccent,
+                    shape: BoxShape.circle,
+                  ),
+                ),
               ),
             ],
           ),
